86e949c19e673a9f3a3a2e43b9fe29a28c254fd3
ViktorAls <44008135+ViktorAls@users.noreply.github.com>
2019-02-16 14:14:37 +0300

    user

--- yii2/console/migrations/m190216_095215_create_table.php
+++ yii2/console/migrations/m190216_095215_create_table.php
@@ -14,30 +14,30 @@ class m190216_095215_create_table extends Migration
     {
         $this->createTable('information', [
             'id' => $this->primaryKey(),
-            'name'=>$this->char(100)->notNull()->unique(),
-            'value'=>$this->text()->notNull(),
-            'comment'=>$this->text()->null()
+            'name' => $this->char(100)->notNull()->unique(),
+            'value' => $this->text()->notNull(),
+            'comment' => $this->text()->null()
         ]);
         $this->createTable('lesson', [
             'id' => $this->primaryKey(),
-            'name'=>$this->char(100)->notNull()
+            'name' => $this->char(100)->notNull()
         ]);
         $this->createTable('comment', [
-                'id'=>$this->primaryKey(),
-                'id_post'=>$this->integer()->notNull(),
-                'id_parent'=>$this->integer()->null(),
-                'text'=>$this->text()->notNull(),
-                'created_at'=>$this->dateTime()->null(),
-                'id_user'=>$this->integer()->notNull(),
-            ]);
+            'id' => $this->primaryKey(),
+            'id_post' => $this->integer()->notNull(),
+            'id_parent' => $this->integer()->null(),
+            'text' => $this->text()->notNull(),
+            'created_at' => $this->dateTime()->null(),
+            'id_user' => $this->integer()->notNull(),
+        ]);
         $this->createTable('document', [
             'id' => $this->primaryKey(),
-            'name'=>$this->text()->notNull(),
-            'href'=>$this->text()->notNull(),
-            'description'=>$this->text()->notNull(),
-            'updated_at'=>$this->integer()->notNull(),
-            'created_at'=>$this->integer()->notNull(),
-            'id_lesson'=>$this->integer()->notNull(),
+            'name' => $this->text()->notNull(),
+            'href' => $this->text()->notNull(),
+            'description' => $this->text()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'id_lesson' => $this->integer()->notNull(),
         ]);
         $this->createTable('post', [
             'id' => $this->primaryKey(),
@@ -47,16 +47,16 @@ class m190216_095215_create_table extends Migration
             'updated_at' => $this->integer()->notNull(),
             'type' => $this->integer()->notNull(),
             'icon' => $this->text()->notNull(),
-            'id_lesson'=> $this->integer()->notNull(),
+            'id_lesson' => $this->integer()->notNull(),
         ]);
         $this->createTable('group', [
             'id' => $this->primaryKey(),
-            'name'=>$this->char(100)->notNull()
+            'name' => $this->char(100)->notNull()
         ]);
         $this->createTable('postFile', [
             'id' => $this->primaryKey(),
-            'name'=>$this->text()->notNull(),
-            'id_post'=>$this->integer()->notNull(),
+            'name' => $this->text()->notNull(),
+            'id_post' => $this->integer()->notNull(),
         ]);
         $this->createTable('tagDocument', [
             'id_document' => $this->integer(),
@@ -68,45 +68,55 @@ class m190216_095215_create_table extends Migration
             'id_tag' => $this->integer(),
             'PRIMARY KEY(id_post, id_tag)',
         ]);
-        $this->createTable('tag',[
-            'id'=>$this->primaryKey(),
-            'name'=>$this->char(50)->notNull()
+        $this->createTable('tag', [
+            'id' => $this->primaryKey(),
+            'name' => $this->char(50)->notNull()
         ]);
+        
+        // таблица  document, поле id_lesson
         $this->createIndex(
             'idx-document-id_lesson',
             'document',
             'id_lesson'
         );
+        // таблица  post, поле id_lesson
         $this->createIndex(
             'idx-post-id_lesson',
             'post',
             'id_lesson'
         );
+        // таблица  tagPost, поле id_post
         $this->createIndex(
             'idx-tagPost-id_post',
             'tagPost',
             'id_post'
         );
+        // таблица  postFile, поле id_post
         $this->createIndex(
             'idx-postFile-id_post',
             'postFile',
             'id_post'
         );
+        // таблица  tagPost, поле id_tag
         $this->createIndex(
             'idx-tagPost-id_tag',
             'tagPost',
             'id_tag'
         );
+        // таблица  tagDocument, поле id_document
         $this->createIndex(
             'idx-tagDocument-id_document',
             'tagDocument',
             'id_document'
         );
+        // таблица  tagDocument, поле id_tag
         $this->createIndex(
             'idx-tagDocument-id_tag',
             'tagDocument',
             'id_tag'
         );
+
+        // связь табилци document по полю id_lesson к табилци lesson полю id
         $this->addForeignKey(
             'fk-document-id_lesson',
             'document',
@@ -115,6 +125,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци post по полю id_lesson к табилци lesson полю id
         $this->addForeignKey(
             'fk-post-id_lesson',
             'post',
@@ -123,6 +134,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци postFile по полю id_post к табилци post полю id
         $this->addForeignKey(
             'fk-postFile-id_post',
             'postFile',
@@ -131,6 +143,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци tagPost по полю id_post к табилци post полю id
         $this->addForeignKey(
             'fk-tagPost-id_post',
             'tagPost',
@@ -139,6 +152,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци tagPost по полю id_tag к табилци tag полю id
         $this->addForeignKey(
             'fk-tagPost-id_tag',
             'tagPost',
@@ -147,6 +161,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци tagDocument по полю id_document к табилци document полю id
         $this->addForeignKey(
             'fk-tagDocument-id_document',
             'tagDocument',
@@ -155,6 +170,7 @@ class m190216_095215_create_table extends Migration
             'id',
             'CASCADE'
         );
+        // связь табилци tagDocument по полю id_tag к табилци tag полю id
         $this->addForeignKey(
             'fk-tagDocument-id_tag',
             'tagDocument',
@@ -170,70 +186,87 @@ class m190216_095215_create_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropIndex(
-            'idx-tagDocument-id_lesson',
-            'post'
-        );
-        $this->dropIndex(
-            'idx-postFile-id_post',
-            'postFile'
-        );
-        $this->dropIndex(
-            'idx-document-id_lesson',
-            'document'
-        );
-        $this->dropIndex(
-            'idx-tagDocument-id_document',
-            'tagDocument'
-        );
-        $this->dropIndex(
-            'idx-tagPost-id_post',
-            'tagPost'
-        );
-        $this->dropIndex(
-            'idx-tagPost-id_tag',
-            'tagPost'
-        );
-        $this->dropIndex(
-            'idx-tagDocument-id_tag',
-            'tagDocument'
-        );
+        //внешний ключ для таблици postFile  поле id_post
         $this->dropForeignKey(
             'fk-postFile-id_post',
             'postFile'
         );
+        //внешний ключ для таблици post поле id_lesson
         $this->dropForeignKey(
             'fk-post-id_lesson',
             'post'
         );
+        //внешний ключ для таблици document поле id_lesson
         $this->dropForeignKey(
             'fk-document-id_lesson',
             'document'
         );
+        //внешний ключ для таблици tagPost поле id_tag
         $this->dropForeignKey(
             'fk-tagPost-id_tag',
             'tagPost'
         );
+        //внешний ключ для таблици tagPost поле id_post
         $this->dropForeignKey(
             'fk-tagPost-id_post',
             'tagPost'
         );
+        //внешний ключ для таблици tagDocument поле id_document
         $this->dropForeignKey(
             'fk-tagDocument-id_document',
             'tagDocument'
         );
+        //внешний ключ для таблици tagDocument поле id_tag
         $this->dropForeignKey(
             'fk-tagDocument-id_tag',
             'tagDocument'
         );
-        $this->dropTable('postFile');
+
+        //индекс таблици document поля id_lesson
+        $this->dropIndex(
+            'idx-document-id_lesson',
+            'document'
+        );
+        //индекс таблици post поля id_lesson
+        $this->dropIndex(
+            'idx-post-id_lesson',
+            'post'
+        );
+        //индекс таблици tagPost поля id_post
+        $this->dropIndex(
+            'idx-tagPost-id_post',
+            'tagPost'
+        );
+        //индекс таблици postFile поля id_post
+        $this->dropIndex(
+            'idx-postFile-id_post',
+            'postFile'
+        );
+        //индекс таблици tagPost поля id_tag
+        $this->dropIndex(
+            'idx-tagPost-id_tag',
+            'tagPost'
+        );
+        //индекс таблици tagDocument поля id_document
+        $this->dropIndex(
+            'idx-tagDocument-id_document',
+            'tagDocument'
+        );
+        //индекс таблици tagDocument поля id_tag
+        $this->dropIndex(
+            'idx-tagDocument-id_tag',
+            'tagDocument'
+        );
+
+        $this->dropTable('tag');
         $this->dropTable('post');
-        $this->dropTable('information');
         $this->dropTable('group');
         $this->dropTable('lesson');
         $this->dropTable('comment');
-        $this->dropTable('document');
         $this->dropTable('tagPost');
+        $this->dropTable('postFile');
+        $this->dropTable('document');
+        $this->dropTable('information');
         $this->dropTable('tagDocument');
     }
 
--- yii2/console/migrations/m190216_104002_insert_information_table.php
+++ yii2/console/migrations/m190216_104002_insert_information_table.php
@@ -7,22 +7,34 @@ use yii\db\Migration;
  */
 class m190216_104002_insert_information_table extends Migration
 {
+    /**
+     * @return bool|void
+     * @throws \yii\db\Exception
+     */
     public function safeUp()
     {
-        Yii::$app->db->createCommand()->batchInsert('country', ['name',''], [
-            ['Австралия'],
-            ['Австрия'],
-            ['Азербайджан'],
-            ['Албания'],
-        ])->execute();
+        Yii::$app->db->createCommand()->batchInsert('information', ['name','value','comment'],
+            [
+                ['vkontakte','http://vk.com/id1','Страница ВКонтакте'],
+                ['mail','mail@ru.ru','Почта для ображращения'],
+                ['faecbook','страница в файсбуке','страница в файсбуке'],
+                ['pointOfView','Моя точка зрения','Моя точка зрения'],
+                ['AboutMe','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Страница О себе. Главная запись'],
+                ['whoAmI','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Страница О себе. Кто я'],
+                ['myMission','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Страница О себе. Моя миссия'],
+                ['myOpinion','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Страница О себе. Мой взгляд'],
+                ['myValues','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Страница О себе. Мой ценности'],
+                ['mainFeedback','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Главный текст на странице обратная связь'],
+                ['address','Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi','Адрес, где можно найти'],
+            ]
+        )->execute();
 
     }
 
     public function safeDown()
     {
-        Yii::$app->db->createCommand()->delete('country', ['in', 'name', ['Австралия', 'Австрия', 'Азербайджан', 'Албания']]
-        )->execute();
-
+       echo 'Сделанные действия отмене не подлежат';
+       return false;
     }
 
 }
